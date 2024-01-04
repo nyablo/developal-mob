7583c37ffdc5c7d152411902d6b09e0acd97a1a1
Nazarii Yablonskyi <nazarii.yablonskyi@gmail.com>
2024-01-03 19:24:15 -0800

    upd bundle id android

--- android/app/src/main/java/com/nazariiyablonskyi/devcon/MainActivity.kt
+++ android/app/src/main/java/com/nazariiyablonskyi/devcon/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.contacts
+package com.nazariiyablonskyi.devcon
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/nazariiyablonskyi/devcon/MainApplication.kt
+++ android/app/src/main/java/com/nazariiyablonskyi/devcon/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.contacts
+package com.nazariiyablonskyi.devcon
 
 import android.app.Application
 import com.facebook.react.PackageList
